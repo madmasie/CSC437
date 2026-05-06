b9fad3fc403a17e963139086a0ef4e4e4033f748
Your Name <you@example.com>
2026-05-06 12:40:59 -0700

    lab 10

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -1,20 +1,10 @@
 import express from "express";
-import Recipes from "./services/recipe-svc.js";
 const app = express();
 const port = process.env.PORT || 3000;
 const staticDir = process.env.STATIC || "public";
 app.use(express.static(staticDir));
-app.use(express.json());
 app.get("/hello", (req, res) => {
     res.send("Hello, World");
-});
-app.get("/api/recipes/:id", (req, res) => {
-    const id = req.params.id;
-    const data = Recipes.get(id);
-    if (data)
-        res.send(data);
-    else
-        res.status(404).send();
 });
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
